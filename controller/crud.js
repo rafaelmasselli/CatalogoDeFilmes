9d24c8c810fafda05705ae2cd46a945cefef882f
rafaelmasselli <rafaelmasselli0@gmail.com>
2021-10-20 14:48:07 -0300

    mvc

---
+++ controller/crud.js
@@ -0,0 +1,110 @@
+const express = require("express");
+const router = express.Router();
+const filmes = require('.././model/filme');
+
+let mensagem = ""
+
+router.get("/",  async (req, res) => {
+  const filme = await filmes.findAll();
+    setTimeout(() => { mensagem = "";}, 5000);
+  res.render("../views/index", {mensagem,filme});
+});
+
+// cadastro
+
+router.get("/cadastro", (req, res) => {
+  res.render("../views/cadastro");
+});
+
+// cadastro do render
+  
+router.post("/New", async (req, res) => {
+
+  const {nome,genero,image,diretor,ano} = req.body;
+  const filme = await filmes.create({
+    nome:nome,
+    genero:genero,
+    imagem:image,
+    diretor:diretor,
+    ano:ano,
+  })
+  mensagem = `O Filme ${nome} foi adicionado`
+res.redirect("/"),filme})
+
+router.get("/detalhes/:id", async function (req, res){
+  const filme = await filmes.findByPk(req.params.id);
+  res.render("../views/detalhes",{filme:filme})
+});
+
+// deletar do render
+
+router.get("/deletar/:id", async (req, res) => {
+  const filme = await filmes.findByPk(req.params.id);
+
+  if (!filme) {
+    res.render("../views/deletar", {
+      mensagem: "Filme não encontrado!",
+    });
+  }
+
+  res.render("../views/deletar", {
+    filme,
+  });
+});
+
+router.post('/deletar/deletar/:id', async (req,res) => {
+  const filme = await filmes.findByPk(req.params.id);
+
+  if (!filme) {    
+    res.render("../views/deletar",  {mensagem: "Filme não encontrado!",});};
+
+  await filme.destroy();
+
+  const filmesList = await filmes.findAll();
+  res.render("../views/index", {mensagem: `Filme deletado com sucesso!`,  filme:filmesList});
+});
+
+
+// editar do render
+
+router.get('/editar/:id', async (req,res) => {
+  const filme = await filmes.findByPk(req.params.id);
+
+  if (!filme) {
+    res.render("../views/deletar", {
+      mensagem: "Filme não encontrado!",
+    });
+  }
+  
+  var options = [ 
+    "Animação", "Comédia", "Comédia Romântica", "Comédia Dramática", "Documentário", "Drama","Faroeste", "Ficção Científica", "Musical", "Suspense", "Terror / Horror"];
+  for ( var i = 0; i < options.length; i++ )
+  {
+      var selected = (filme.genero == i ) ? "selected" : "";
+  }
+
+  res.render("../views/editar", {filme:filme});
+});
+
+
+// editar no render 
+
+router.post("/editar/:id", async function (req,res){
+    const filme = await filmes.findByPk(req.params.id);
+    const { nome, genero, image, diretor, ano} = req.body;
+    
+    filme.id = req.params.id;
+    filme.nome = nome;
+    filme.genero = genero;
+    filme.imagem = image;
+    filme.diretor = diretor;
+    filme.ano = ano;
+
+    mensagem = `O Filme ${nome} foi alterado com sucesso!`
+  
+    await filme.save();
+    res.render("../views/editar",{filme,mensagem});
+});
+
+
+module.exports = router
--- index.js
+++ index.js
@@ -7,116 +7,13 @@ const porta = process.env.PORT || 3000;
 const db = require('./model/database')
 const filmes = require('./model/filme');
 
-let mensagem = ""
-
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "/views/public")));
 app.use(express.urlencoded({ extended: true }))
 
-// render com a mensagem 
+// rota do crud
 
-
-app.get("/",  async (req, res) => {
-  const filme = await filmes.findAll();
-    setTimeout(() => { mensagem = "";}, 5000);
-  res.render("index", {mensagem,filme});
-});
-
-// render cadastro
-
-app.get("/cadastro", (req, res) => {
-  res.render("../views/cadastro");
-});
-
-// render cadastro, redirect index principal
-
-app.post("/New", async (req, res) => {
-
-  const {nome,genero,image,diretor,ano} = req.body;
-  const filme = await filmes.create({
-    nome:nome,
-    genero:genero,
-    imagem:image,
-    diretor:diretor,
-    ano:ano,
-  })
-
-
-  mensagem = `O Filme ${nome} foi adicionado`
-res.redirect("/"),filme})
-
-// render detalhes com id 
-
-app.get("/detalhes/:id", async function (req, res){
-  const filme = await filmes.findByPk(req.params.id);
-  res.render("../views/detalhes",{filme:filme})
-});
-
-// render deleter
-
-app.get("/deletar/:id", async (req, res) => {
-  const filme = await filmes.findByPk(req.params.id);
-
-  if (!filme) {
-    res.render("deletar", {
-      mensagem: "Filme não encontrado!",
-    });
-  }
-
-  res.render("deletar", {
-    filme,
-  });
-});
-
-app.post('/deletar/deletar/:id', async (req,res) => {
-  const filme = await filmes.findByPk(req.params.id);
-
-  if (!filme) {    
-    res.render("deletar",  {mensagem: "Filme não encontrado!",});};
-
-  await filme.destroy();
-
-  const filmesList = await filmes.findAll();
-  res.render("index", {mensagem: `Filme deletado com sucesso!`,  filme:filmesList});
-});
-
-//render editar
-
-app.get('/editar/:id', async (req,res) => {
-  const filme = await filmes.findByPk(req.params.id);
-
-  if (!filme) {
-    res.render("deletar", {
-      mensagem: "Filme não encontrado!",
-    });
-  }
-  
-  var options = [ 
-    "Animação", "Comédia", "Comédia Romântica", "Comédia Dramática", "Documentário", "Drama","Faroeste", "Ficção Científica", "Musical", "Suspense", "Terror / Horror"];
-  for ( var i = 0; i < options.length; i++ )
-  {
-      var selected = (filme.genero == i ) ? "selected" : "";
-  }
-
-  res.render("../views/editar", {filme:filme});
-});
-
-app.post("/editar/:id", async function (req,res){
-    const filme = await filmes.findByPk(req.params.id);
-    const { nome, genero, image, diretor, ano} = req.body;
-    
-    filme.id = req.params.id;
-    filme.nome = nome;
-    filme.genero = genero;
-    filme.imagem = image;
-    filme.diretor = diretor;
-    filme.ano = ano;
-
-    mensagem = `O Filme ${nome} foi alterado com sucesso!`
-  
-    await filme.save();
-    res.render("../views/editar",{filme,mensagem});
-});
+app.use("/", require("./controller/crud"));
 
 db.conectado();
 app.listen(porta, () =>console.log(`Servidor rodando em http://localhost:${porta}`));
