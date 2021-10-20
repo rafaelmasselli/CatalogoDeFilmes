8c87f23320637326adffa00c05dbb41b1f001091
rafaelmasselli <rafaelmasselli0@gmail.com>
2021-10-20 02:03:34 -0300

    require

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const path = require("path");
 const app = express();
 require('dotenv').config()
-const yup = require('yup');
 
 const porta = process.env.PORT || 3000;
 const db = require('./model/database')
